117b82ff20ff454d821f03a17bb2f36a6621d9cc
Evgeny Tarasov <eatarasov@yahoo.com>
2021-12-17 15:10:53 +0300

    Post Repository

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/repositories/PostRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.blog.repositories;
+
+import com.springboot.blog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
