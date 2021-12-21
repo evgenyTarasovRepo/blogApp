a51a8cc1d787dda5c29c3b6793684a901fa248bc
Evgeny Tarasov <eatarasov@yahoo.com>
2021-12-21 13:39:57 +0300

    get all posts

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
@@ -5,13 +5,12 @@ import com.springboot.blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("api/post/posts")
+@RequestMapping("api/posts")
 public class PostController {
 
     private PostService postService;
@@ -25,5 +24,11 @@ public class PostController {
     @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    //get all posts rest api
+    @GetMapping()
+    public List<PostDto> getAllPosts() {
+        return postService.getAllPosts();
     }
 }
