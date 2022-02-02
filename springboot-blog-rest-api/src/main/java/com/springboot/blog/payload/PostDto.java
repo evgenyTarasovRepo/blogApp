021415406f57a847b974df331a14801601494b2d
Evgeny Tarasov <eatarasov@yahoo.com>
2022-02-02 11:28:41 +0300

    added validation, added basic auth

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/PostDto.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/PostDto.java
@@ -2,13 +2,24 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class PostDto {
     private long id;
-    private String title;
-    private String description;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
+    private String title; //should be not null or empty and have at least 2 chars
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
+    private String description; //should be not null or empty and have at least 10 chars
+
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
